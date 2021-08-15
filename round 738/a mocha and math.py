94f2457ed9cf0fff693d95b47d97ac261866d99d
mohamed-reda <mohamed.reda.007007@gmail.com>
2021-08-15 19:08:57 +0200

    push to dev

--- round 738/a mocha and math.py
+++ round 738/a mocha and math.py
@@ -3,10 +3,23 @@ n = int(input())
 
 def bitwiseAND(my_list, max_num):
     # get the combination
-    for i in range(len(my_list)):
+    max_com = 0
+    first = 0
+    second = 0
+    # for i in range(int(len(my_list) / 2)):
+    #     com = my_list[i] + my_list[len(my_list) - 1 - i]
+    #     if max_com < com:
+    #         max_com = com
+    #     first = my_list[i]
+    #     second = my_list[len(my_list) - 1 - i]
+    # print(first & second)
+    for i in range(int(len(my_list) / 2)):
+
+        # print(f"{my_list[i]} & {my_list[len(my_list) - 1 - i]} is : {my_list[i] & my_list[len(my_list) - 1 - i]}")
         if my_list[i] == max_num or my_list[len(my_list) - 1 - i] == max_num:
             print(my_list[i] & my_list[len(my_list) - 1 - i])
             return
+    # # print('-------------------------')
 
 
 for i in range(n):
@@ -16,14 +29,15 @@ for i in range(n):
     max_num = 0
     for ofs in range(testcase):
         current = int(list[ofs])
-        if max_num < current:
+        if max_num < current and ofs > 0:
             max_num = current
-            
+
         my_list.append(current)
     if testcase % 2 == 0:
         bitwiseAND(my_list, max_num)
     else:
         bitwiseAND(my_list[1:testcase], max_num)
+
 """
 4
 2
